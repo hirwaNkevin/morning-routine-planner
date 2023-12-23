eeebd5a187c62591e9f7a5a0fd3b4e611e3aa973
hirwaNkevin <hirwankevin@gmail.com>
2023-12-23 14:39:47 +0200

    add functionality to function

--- wakeup_and_bedtime.py
+++ wakeup_and_bedtime.py
@@ -2,18 +2,13 @@
 
 from datetime import datetime, date, time, timedelta, timezone
 
-
-# inputs
-
-# This is when you need to leave
-# for work. It’s when your morning routine needs to end.
-# start_time = datetime.strptime("05:00 am", '%I:%M %p')
+# start_day_time = datetime.strptime("05:00 am", '%I:%M %p')
 
 # sleep_hours = timedelta(hours=8)
 
 # routine_duration = timedelta(hours=2)
 
-# wake_time = (start_time+routine_duration).strftime('%I:%M %p')
+# wake_time = (start_day_time+routine_duration).strftime('%I:%M %p')
 # bed_time = (datetime.strptime(wake_time, '%I:%M %p') -
 #             sleep_hours).strftime('%I:%M %p')
 
@@ -21,19 +16,33 @@ from datetime import datetime, date, time, timedelta, timezone
 # print(bed_time)
 
 
-def wake_and_bed(start_time: str, sleep_hours: int, routine_duration: int) -> list:
+def wake_and_bed(start_day_time: int, sleep_hours_int: int, routine_duration_int: int) -> dict:
     """
     Calculate the wake hour and bed hour based on the desired 
     start time and duration of the morning routine; and the desired sleep hours.
 
     Parameters:
-    - start_time (str): the hour at which the morning routine will start in the format 'II:MM AM/PM'
+    - start_day_time (int): the hour at which the morning routine will start in the format 'II:MM AM/PM'
     - sleep_hours (int): the desired hours of sleep.
     - routine_duration (int): the duration of the morning routine
 
     returns:
-    - [wake_time (str),bed_time (str)] (list)
+    - [wake_time (str),bed_time (str)] (dict)
 
     """
+    # Validate inputs
 
-    return []
+    # Convert inputs
+    # start_day_time = datetime.strptime(start_day_time_string, '%I:%M:%S %p')
+    # Using an arbitrary date for calculation
+    start_day_time = bedtime = datetime(2023, 1, 1, start_day_time, 0)
+    sleep_hours = timedelta(hours=sleep_hours_int)
+    routine_duration = timedelta(hours=routine_duration_int)
+
+    # Calculate wake and bed time
+    wake_time = start_day_time-routine_duration
+    bed_time = wake_time - sleep_hours
+
+    # Format outputs
+
+    return {"wake time": wake_time.strftime('%I:%M:%S %p'), "bed time": bed_time.strftime('%I:%M:%S %p')}
